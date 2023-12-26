f9a966a67bcfe3f5f57878891316ffb21b7088a8
HugzerDT <hugo.lim@datatilt.fr>
2023-12-26 17:32:51 +0100

    test en java

---
+++ test_machin.java
@@ -0,0 +1,13 @@
+/**
+ * test_machin
+ */
+public class test_machin {
+
+    public static void main(String[] args) {
+        machin();
+    }
+
+    public static void machin() {
+        System.out.println("Hello World!");
+    }
+}
